4b6ab16323399d5eb685b7fe574a5197ad84361b
kristinadhakal <112728281+kristinadhakal@users.noreply.github.com>
2022-10-27 12:20:47 -0600

    ello
    hi

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hi");
+// Colour Array by Kristina
+
+// Variables forHTML elelmets
+let containerEL = document.getElementById("container");
+
+let colours = ["red", "green", "blue"];
+// index ->     0        1       2
+
+containerEL.innerHTML = "<div>" + colours[0] + "</div>";
+console.log(colours[0]);
