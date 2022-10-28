1fc9e06684911102ec610f41edaa97113b3cc989
kristinadhakal <112728281+kristinadhakal@users.noreply.github.com>
2022-10-27 20:56:33 -0600

    10/27/22
    e

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("hi");
+// Colour Array by kristina
+
+// VAriables for HYML Elements 
+let containerEL = document.getElementById("container");
+
+
+let colours = ["red", "green", "blue"];
+
+
+
+containerEL. innerHTML = "<div style = 'background: '" + colours[0] + "</div>"
