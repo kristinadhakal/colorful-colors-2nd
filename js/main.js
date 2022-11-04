d121f50872aa6ee2370bdca8115cc2d134f83295
kristinadhakal <112728281+kristinadhakal@users.noreply.github.com>
2022-11-04 14:52:15 -0600

    hu
    hayyy

--- js/main.js
+++ js/main.js
@@ -2,25 +2,52 @@
 
 // VAriables for HYML Elements
 let containerEL = document.getElementById("container");
+let inputEl = document.getElementById("colour-in");
+
+// Global VAriable
+let colours;
+
+/// Fetch contains from colours.txt
+fetch("colors.txt").then(convertData).then(processData);
+
+function convertData(rawData) {
+  return rawData.text();
+}
+
+function processData(stringData) {
+  colours = stringData.split(/\r?\n/);
+  displayColours(colours);
+}
+
+console.log("After Fetch");
+
+// EVenet Listener
+inputEl.addEventListener("keydown", submitHandler);
+
+function submitHandler(event) {
+  if (event.keyCode === 13) {
+    // Add user's colours to the colours array and display
+    colours.push(inputEl.value);
+    inputEl.value = "";
+    displayColours(colours);
+  }
+}
+
+function displayColours(colours) {
+  // Display colours on page
+  let divStr = "";
+  let count = 0;
+
+  for (let i = 0; i < colours.length; i++) {
+    if (colours[i].length === 4) {
+      divStr += `<div style ="background: ${colours[i]}">${colours[i]}</div>`;
+      count++;
+    }
+  }
+  containerEL.innerHTML = divStr + `<p> Count: ${count}</p>`;
+}
 
 // array
-let colours = ["red", "green", "blue", "orange", "cyan", "purple"];
-
-// lcick event
-document.addEventListener("click", update);
-
-// function update() {
-//   containerEL.innerHTML = `<div style = "background: ${
-//     colours[randomInt(0, colours.length)]
-//   }"></div>`;
-// }
-
-// or
-
-function update() {
-  let randCol = randomElement(colours);
-  containerEL.innerHTML = `<div style = "background: ${randCol}"></div>`;
-}
 
 // Access lenth of array
 // console.log(colours.length)
